47526a5fd669ce55e3ceab8c3f88f390acd4808f
irawanrafidzufar <irawanrafidzufar@gmail.com>
2025-06-22 17:12:11 +0700

    First Commit (9:25/2:01:03)

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--=============== FAVICON ===============-->
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+
+    <!--=============== REMIXICONS ===============-->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+
+    <!--=============== CSS ===============-->
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+
+    <title>Coffee website - Jupyter</title>
+</head>
+
+<body>
+    <!--==================== HEADER ====================-->
+    <header class="header" id="header">
+        <div class="header__border"></div>
+    </header>
+
+    <!--==================== MAIN ====================-->
+    <main class="main">
+        <!--==================== HOME ====================-->
+        <section class="home section" id="home">
+
+        </section>
+
+        <!--==================== PRODUCTS ====================-->
+        <section class="products">
+
+        </section>
+
+        <!--==================== ABOUT ====================-->
+        <section class="about" id="about">
+
+        </section>
+
+        <!--==================== STEPS ====================-->
+        <section class="steps" id="steps">
+
+        </section>
+
+        <!--==================== TESTIMONIAL ====================-->
+        <section class="testimonial" id="testimonial">
+
+        </section>
+    </main>
+
+    <!--==================== FOOTER ====================-->
+    <footer class="footer">
+
+    </footer>
+
+    <!--========== SCROLL UP ==========-->
+
+
+    <!--=============== SCROLLREVEAL ===============-->
+    <script src=""></script>
+
+    <!--=============== MAIN JS ===============-->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+</body>
+
+</html>
