e0d5eb85c4740e9bd71c5a1d4d35eff63bcc6f10
irawanrafidzufar <irawanrafidzufar@gmail.com>
2025-06-24 21:34:01 +0700

    Fourth Commit (1:26:48/2:01:03)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
                 <ul class="nav__list">
 
                     <li class="nav__item">
-                        <a href="#home" class="nav__link">
+                        <a href="#home" class="nav__link active-link">
                             <i class="ri-home-5-fill"></i>
                             <span>Home</span>
                         </a>
@@ -163,22 +163,152 @@
 
         <!--==================== STEPS ====================-->
         <section class="steps" id="steps">
+            <div class="steps__bg section">
+                <h2 class="section__title">
+                    Steps of manufacturing our products
+                </h2>
 
+                <div class="steps__container container">
+                    <img src="{{ asset('assets/images/coffee-beans-bg.png') }}" alt="Steps image" class="steps__bg-img">
+
+                    <div class="steps__content">
+                        <img src="{{ asset('assets/images/steps-curve-line.svg') }}" alt="Steps image"
+                            class="steps__border">
+
+                        <div class="steps__card">
+                            <div class="steps__circle">
+                                <div class="steps__subcircle">
+                                    01
+                                </div>
+
+                                <img src="{{ asset('assets/images/steps-green-coffee.png') }}" alt="Steps image"
+                                    class="steps__img">
+                            </div>
+
+                            <p class="steps__description">
+                                Harvest occurs annually when the coffee
+                                beans reach maturity and are collected
+                                for processing.
+                            </p>
+                        </div>
+
+                        <div class="steps__card steps__card-move">
+                            <div class="steps__circle">
+                                <div class="steps__subcircle">
+                                    02
+                                </div>
+
+                                <img src="{{ asset('assets/images/steps-coffee-beans.png') }}" alt="Steps image"
+                                    class="steps__img">
+                            </div>
+
+                            <p class="steps__description">
+                                The beans are dried using a wet or dry
+                                technique, depending on the taste we want
+                                to obtain.
+                            </p>
+                        </div>
+
+                        <div class="steps__card">
+                            <div class="steps__circle">
+                                <div class="steps__subcircle">
+                                    03
+                                </div>
+
+                                <img src="{{ asset('assets/images/steps-ground-coffee.png') }}" alt="Steps image"
+                                    class="steps__img">
+                            </div>
+
+                            <p class="steps__description">
+                                The coffee is roasted and acquires
+                                its flavor by processing the grain
+                                in ovens.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <!--==================== TESTIMONIAL ====================-->
         <section class="testimonial" id="testimonial">
+            <div class="testimonial__bg section">
+                <div class="testimonial__container container grid">
+                    <div class="testimonial__data">
+                        <h2 class="section__title">
+                            Coffee is the best drink in the
+                            morning, it keeps you focused.
+                        </h2>
 
+                        <span class="testimonial__name">John Doe</span>
+                    </div>
+
+                    <img src="{{ asset('assets/images/testimonial-coffee.png') }}" alt="Testimonial Coffee"
+                        class="testimonial__img">
+                </div>
+            </div>
         </section>
     </main>
 
     <!--==================== FOOTER ====================-->
     <footer class="footer">
+        <div class="footer__bg">
+            <img src="{{ asset('assets/images/coffee-beans-bg.png') }}" alt="footer image" class="footer__bg-img">
 
+            <div class="footer__container container grid">
+                <div class="footer__data grid">
+                    <div>
+                        <a href="#" class="footer__logo">
+                            <img src="{{ asset('assets/images/logo.png') }}" alt="Logo">
+                            CoffeeShop
+                        </a>
+
+                        <h3 class="footer__title">
+                            Sign up for our newsletter
+                        </h3>
+                    </div>
+
+                    <form action="" class="footer__form grid">
+                        <input type="email" placeholder="Enter e-mail address" class="footer__input">
+
+                        <button class="button footer__button" type="submit">
+                            Subscribe <i class="ri-arrow-right-s-line"></i>
+                        </button>
+
+                        <p class="footer__description">
+                            We care about your data. Read our
+                            <a href="#" class="footer__privacy">Privacy Policy</a>
+                        </p>
+                    </form>
+                </div>
+
+                <div class="footer__content grid">
+                    <div class="footer__social">
+                        <a href="https://www.facebook.com/" target="_blank" class="footer__social-link">
+                            <i class="ri-facebook-fill"></i>
+                        </a>
+
+                        <a href="https://www.instagram.com/" target="_blank" class="footer__social-link">
+                            <i class="ri-instagram-fill"></i>
+                        </a>
+
+                        <a href="https://twitter.com/" target="_blank" class="footer__social-link">
+                            <i class="ri-twitter-x-fill"></i>
+                        </a>
+                    </div>
+
+                    <span class="footer__copy">
+                        &#169; Copyright Jupyter. All rights reserved
+                    </span>
+                </div>
+            </div>
+        </div>
     </footer>
 
     <!--========== SCROLL UP ==========-->
-
+    <a href="#" class="scrollup" id="scroll-up">
+        <i class="ri-arrow-up-line"></i>
+    </a>
 
     <!--=============== SCROLLREVEAL ===============-->
     <script src=""></script>
